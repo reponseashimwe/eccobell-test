9ecfb1e32aedf08969603c4926c37cb7b66efb8e
Reponse Ashimwe <reponseashime@gmail.com>
2024-11-03 22:34:41 +0200

    landing page

---
+++ src/assets/icons/HouseKeepingIcon.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function HouseKeepingIcon() {
+  return (
+    <div>
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clip-path="url(#clip0_718_207)">
+          <g clip-path="url(#clip1_718_207)">
+            <path
+              d="M16.6667 8.33366V5.83366C16.6667 4.91699 15.9167 4.16699 15.0001 4.16699H5.00008C4.08341 4.16699 3.33341 4.91699 3.33341 5.83366V8.33366C2.41675 8.33366 1.66675 9.08366 1.66675 10.0003V14.167H2.77508L3.33341 15.8337H4.16675L4.72508 14.167H15.2834L15.8334 15.8337H16.6667L17.2251 14.167H18.3334V10.0003C18.3334 9.08366 17.5834 8.33366 16.6667 8.33366ZM9.16675 8.33366H5.00008V5.83366H9.16675V8.33366ZM15.0001 8.33366H10.8334V5.83366H15.0001V8.33366Z"
+              fill="black"
+            />
+          </g>
+        </g>
+        <defs>
+          <clipPath id="clip0_718_207">
+            <rect width="20" height="20" fill="white" />
+          </clipPath>
+          <clipPath id="clip1_718_207">
+            <rect
+              width="20"
+              height="20"
+              fill="white"
+              transform="translate(0 -0.00683594)"
+            />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+export default HouseKeepingIcon;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,32 @@ export default {
     extend: {
       colors: {
         blue: "#197594",
+        grayBg: "#F5F5F3",
+      },
+      width: {
+        "content-row": "100%",
+        "content-div": "95%", // default width
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".content-row": {
+          display: "flex",
+          width: "100%",
+          justifyContent: "center",
+        },
+        ".content-div": {
+          width: "95%",
+          "@screen md": {
+            width: "90%",
+          },
+          "@screen lg": {
+            width: "80%",
+          },
+        },
+      });
+    },
+  ],
 };
